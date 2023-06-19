afd5fd667971b1f146719928f64f69af03148668
Chris <c.r.zambito@gmail.com>
2023-06-19 19:58:26 -0400

    firebase

--- ghi/src/components/Application.jsx
+++ ghi/src/components/Application.jsx
@@ -1,37 +1,63 @@
-import React, { useState } from "react";
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase";
 
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    middleName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    lastName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+});
+
 function Application() {
-    const [firstName, setFirstName] = useState("");
-    const [middleName, setMiddleName] = useState("");
-    const [lastName, setLastName] = useState("");
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            middleName: '',
+            lastName: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: async (values) => {
+            try {
+                const docRef = await addDoc(collection(db, "application"), {
+                    "first name": values.firstName,
+                    "middle name": values.middleName,
+                    "last name": values.lastName,
+                });
 
-    const submitForm = async (event) => {
-        event.preventDefault();
+                console.log("Document written with ID: ", docRef.id);
 
-        try {
-            const docRef = await addDoc(collection(db, "application"), {
-                "first name": firstName,
-                "middle name": middleName,
-                "last name": lastName,
-            });
-
-            console.log("Document written with ID: ", docRef.id);
-
-            setFirstName("");
-            setMiddleName("");
-            setLastName("");
-        } catch (error) {
-            console.error("Error adding document: ", error);
-        }
-    };
+                formik.resetForm();
+            } catch (error) {
+                console.error("Error adding document: ", error);
+            }
+        },
+    });
 
     return (
-        <form onSubmit={submitForm}>
-            <input value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="First name" />
-            <input value={middleName} onChange={e => setMiddleName(e.target.value)} placeholder="Middle name" />
-            <input value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Last name" />
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input id="firstName" type="text" {...formik.getFieldProps('firstName')} />
+            {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+
+            <label htmlFor="middleName">Middle Name</label>
+            <input id="middleName" type="text" {...formik.getFieldProps('middleName')} />
+            {formik.touched.middleName && formik.errors.middleName ? <div>{formik.errors.middleName}</div> : null}
+
+            <label htmlFor="lastName">Last Name</label>
+            <input id="lastName" type="text" {...formik.getFieldProps('lastName')} />
+            {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+
             <button type="submit">Submit</button>
         </form>
     );
--- ghi/src/components/Login.jsx
+++ ghi/src/components/Login.jsx
@@ -1,56 +1,88 @@
-import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { app } from '../firebase';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+import { app } from "../firebase";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logo from "../assets/logo-color.png";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
     const handleLogin = async () => {
         try {
-        await signInWithEmailAndPassword(auth, email, password);
-        toast.success("Successfully logged in!");
+            await signInWithEmailAndPassword(auth, email, password);
+            toast.success("Successfully logged in!");
         } catch (error) {
-        console.error(error);
-        toast.error("Error logging in!");
+            console.error(error);
+            toast.error("Error logging in!");
         }
     };
 
     const handleGoogleLogin = async () => {
         try {
-        await signInWithPopup(auth, provider);
-        toast.success("Successfully logged in with Google!");
+            await signInWithPopup(auth, provider);
+            toast.success("Successfully logged in with Google!");
         } catch (error) {
-        console.error(error);
-        toast.error("Error logging in with Google!");
+            console.error(error);
+            toast.error("Error logging in with Google!");
         }
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-            <div>
-            <img className="mx-auto h-12 w-auto" src="/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
+                <div className="text-center">
+                    <img
+                        className="mx-auto h-50 w-auto shadow-lg bg-white hover:opacity-75 rounded-lg"
+                        src={logo}
+                        alt="fjz llc"
+                    />
+                    <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+                <input
+                    className="w-full px-3 py-2 mt-8 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    className="w-full px-3 py-2 mt-4 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                    className="w-full mt-4 bg-indigo-600 text-white py-2 px-4 rounded focus:outline-none hover:bg-indigo-700"
+                    type="button"
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
+                <div className="mt-4 text-center text-sm text-gray-600">
+                    Or sign in with
+                </div>
+                <button
+                    className="w-full mt-4 bg-red-500 text-white py-2 px-4 rounded focus:outline-none hover:bg-red-600"
+                    type="button"
+                    onClick={handleGoogleLogin}
+                >
+                    Google
+                </button>
             </div>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
-            type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button" onClick={handleLogin}>Login</button>
-            <div className="mt-4 text-center text-sm text-gray-600" >
-            Or sign in with
-            </div>
-            <button className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button" onClick={handleGoogleLogin}>Google</button>
-        </div>
-        <ToastContainer />
+            <ToastContainer />
         </div>
     );
 };
